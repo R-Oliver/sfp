d9fa43b6c6978f939ed84ddc0a34736ad97eb188
Reed Oliver <rwoliver105@gmail.com>
2026-01-19 04:12:56 +0000

    Hello from GCP

---
+++ kernels/matmul.py
@@ -0,0 +1,109 @@
+import functools
+
+import jax
+import jax.numpy as jnp
+from jax.experimental import pallas as pl
+from jax.experimental.pallas import tpu as pltpu
+import numpy as np
+
+"""
+Example(s) from https://docs.jax.dev/en/latest/pallas/tpu/matmul.html#
+"""
+
+
+def matmul_kernel(x_ref, y_ref, z_ref, acc_ref, *, nsteps):
+  @pl.when(pl.program_id(2) == 0)
+  def _():
+    acc_ref[...] = jnp.zeros_like(acc_ref)
+
+  acc_ref[...] += jnp.dot(
+      x_ref[...], y_ref[...], preferred_element_type=jnp.float32
+  )
+
+  @pl.when(pl.program_id(2) == nsteps - 1)
+  def _():
+    z_ref[...] = acc_ref[...].astype(z_ref.dtype)
+
+
+@functools.partial(jax.jit, static_argnames=['bm', 'bk', 'bn'])
+def matmul(
+    x: jax.Array,
+    y: jax.Array,
+    *,
+    bm: int = 128,
+    bk: int = 128,
+    bn: int = 128,
+):
+  m, k = x.shape
+  _, n = y.shape
+  return pl.pallas_call(
+      functools.partial(matmul_kernel, nsteps=k // bk),
+      grid_spec=pltpu.PrefetchScalarGridSpec(
+        num_scalar_prefetch=0,
+        in_specs=[
+            pl.BlockSpec((bm, bk), lambda i, j, k: (i, k)),
+            pl.BlockSpec((bk, bn), lambda i, j, k: (k, j)),
+        ],
+        out_specs=pl.BlockSpec((bm, bn), lambda i, j, k: (i, j)),
+        scratch_shapes=[pltpu.VMEM((bm, bn), jnp.float32)],
+        grid=(m // bm, n // bn, k // bk),
+      ),
+      out_shape=jax.ShapeDtypeStruct((m, n), x.dtype),
+      compiler_params=pltpu.CompilerParams(
+          dimension_semantics=("parallel", "parallel", "arbitrary")),
+  )(x, y)
+
+m, k, n = 4096, 4096, 4096
+k1, k2 = jax.random.split(jax.random.key(0), 2)
+x = jax.random.normal(k1, (m, k), dtype=jnp.bfloat16)
+y = jax.random.normal(k2, (k, n), dtype=jnp.bfloat16)
+np.testing.assert_array_equal(x @ y, matmul(x, y))
+
+
+import timeit
+
+def matmul_flops(m: int, k: int, n: int):
+  return 2 * m * k * n
+
+def matmul_membw(m: int, k: int, n: int, dtype: jnp.dtype):
+  return (m * k + k * n + m * n) * np.dtype(dtype).itemsize
+
+v5e_flops = 197e12
+v5e_membw = 819e9
+v5e_op_intensity = v5e_flops / v5e_membw  # ~240.5
+
+def benchmark(f, ntrials: int = 100):
+  def run(*args, **kwargs):
+    # Compile function first
+    jax.block_until_ready(f(*args, **kwargs))
+    # Time function
+    result = timeit.timeit(lambda: jax.block_until_ready(f(*args, **kwargs)),
+                           number=ntrials)
+    time = result / ntrials
+    # print(f"Time: {time}")
+    return time
+  return run
+
+def analyze_matmul(m: int, k: int, n: int, dtype: np.dtype,
+                   mm_func):
+  x = jnp.ones((m, k), dtype=dtype)
+  y = jnp.ones((k, n), dtype=dtype)
+  time = benchmark(mm_func)(x, y)
+  print(f"----- {m} x {k} x {n} -----")
+  print("Matmul time: ", time)
+  mm_flops = matmul_flops(m, k, n) / time
+  print("Matmul FLOP/s: ", mm_flops)
+  print(f"FLOP/s utilization: {mm_flops / v5e_flops * 100:.4f}%")
+  print()
+
+print("================bm=128, bk=128, bn=128===================")
+mm = functools.partial(matmul, bm=128, bk=128, bn=128)
+analyze_matmul(1024, 1024, 1024, jnp.bfloat16, mm)
+analyze_matmul(4096, 4096, 4096, jnp.bfloat16, mm)
+analyze_matmul(8192, 8192, 8192, jnp.bfloat16, mm)
+
+print("================bm=512, bk=1024, bn=1024===================")
+mm = functools.partial(matmul, bm=512, bk=1024, bn=1024)
+analyze_matmul(1024, 1024, 1024, jnp.bfloat16, mm)
+analyze_matmul(4096, 4096, 4096, jnp.bfloat16, mm)
+analyze_matmul(8192, 8192, 8192, jnp.bfloat16, mm)
