90bb69c1304cac37bb28501050f075fa30b00401
Reed Oliver <rwoliver105@gmail.com>
2026-03-06 06:33:41 +0000

    Added second pperm xla variant

--- sfp/kernels/sharded/jax_references.py
+++ sfp/kernels/sharded/jax_references.py
@@ -1,5 +1,3 @@
-import functools
-
 import jax
 from jax.experimental import pallas as pl
 from jax.experimental.pallas import tpu as pltpu
@@ -15,12 +13,13 @@ def xla_matmul_1(lhs: jax.Array, rhs: jax.Array) -> jax.Array:
     # Then we want to compute on the data
     # Since we did two full all gathers to start, regular GEMMs
     with jax.named_scope('gemm'):
-        local_out = rhs_full @ lhs_full
+        local_out = lhs_full @ rhs_full
     return local_out
 
 def xla_matmul_2(lhs: jax.Array, rhs: jax.Array) -> jax.Array:
     # All gather the weights over x so that each device contains full copy
-    rhs_full = jax.lax.all_gather(rhs, 'x', axis=0, tiled=True)
+    with jax.named_scope('rhs all_gather'):
+        rhs_full = jax.lax.all_gather(rhs, 'x', axis=0, tiled=True)
     """
     Once we have the full weight matrix on each device, we know that our y_idx
     will let us pluck out columns of inputs, but we need to make sure
@@ -33,29 +32,34 @@ def xla_matmul_2(lhs: jax.Array, rhs: jax.Array) -> jax.Array:
 
     # Using the y-ring axis to determined which col stripe of weights to compute locally
     rhs_slice = jax.lax.dynamic_slice(rhs_full, (this_y * k_block, 0), (k_block, n))
-    local_out = lhs @ rhs_slice
+    with jax.named_scope('gemm'):
+        local_out = lhs @ rhs_slice
     # All Reduce over the y-ring to accumulate partial results
-    out = jax.lax.psum(local_out, 'y')
+    with jax.named_scope('all reduce'):
+        out = jax.lax.psum(local_out, 'y')
     return out
 
 def xla_matmul_3(lhs: jax.Array, rhs: jax.Array) -> jax.Array:
     """
     Use some higher precision numerics to demonstrate accumulation order (fp32)
     """
-    rhs_full = jax.lax.all_gather(rhs, 'x', axis=0, tiled=True)
+    with jax.named_scope('rhs all_gather'):
+        rhs_full = jax.lax.all_gather(rhs, 'x', axis=0, tiled=True)
     this_y = jax.lax.axis_index('y')
     y_len = jax.lax.axis_size('y')
     k, n = rhs_full.shape
     k_block = k // y_len
     rhs_slice = jax.lax.dynamic_slice(rhs_full, (this_y * k_block, 0), (k_block, n))
 
-    local_out = jax.lax.dot_general(
-        lhs, rhs_slice,
-        dimension_numbers=(((1,), (0,)), ((), ())),
-        precision=jax.lax.Precision.HIGHEST,
-        preferred_element_type=jnp.float32,
-    )
-    out = jax.lax.psum(local_out, 'y')
+    with jax.named_scope('f32 gemm'):
+        local_out = jax.lax.dot_general(
+            lhs, rhs_slice,
+            dimension_numbers=(((1,), (0,)), ((), ())),
+            precision=jax.lax.Precision.HIGHEST,
+            preferred_element_type=jnp.float32,
+        )
+    with jax.named_scope('all reduce'):
+        out = jax.lax.psum(local_out, 'y')
     return out
 
 def pperm_xla_matmul(lhs: jax.Array, rhs: jax.Array) -> jax.Array:
@@ -66,15 +70,17 @@ def pperm_xla_matmul(lhs: jax.Array, rhs: jax.Array) -> jax.Array:
   """
   this_x = jax.lax.axis_index('x')
   # Swap the shards along the x mesh axis
-  rhs_remote = jax.lax.ppermute(rhs, 'x', perm=[(0, 1), (1, 0)])
+  with jax.named_scope('ppermute'):
+    rhs_remote = jax.lax.ppermute(rhs, 'x', perm=[(0, 1), (1, 0)])
 
   # Build the shards
-  w_full = jax.lax.cond(
-    this_x == 0,
-    lambda _: jax.lax.concatenate([rhs, rhs_remote], dimension=0),
-    lambda _: jax.lax.concatenate([rhs_remote, rhs], dimension=0),
-    operand=None
-  ) # (K, N)
+  with jax.named_scope('build rhs'):
+    w_full = jax.lax.cond(
+        this_x == 0,
+        lambda _: jax.lax.concatenate([rhs, rhs_remote], dimension=0),
+        lambda _: jax.lax.concatenate([rhs_remote, rhs], dimension=0),
+        operand=None
+    ) # (K, N)
 
   this_y  = jax.lax.axis_index('y')
   y_len = jax.lax.axis_size('y')
@@ -83,6 +89,28 @@ def pperm_xla_matmul(lhs: jax.Array, rhs: jax.Array) -> jax.Array:
   k_block = k // y_len
 
   w_slice = jax.lax.dynamic_slice(w_full, (this_y * k_block, 0), (k_block, n))
-  local_out = lhs @ w_slice
-  out = jax.lax.psum(local_out, 'y')
+  with jax.named_scope('local gemm'):
+    local_out = lhs @ w_slice
+  with jax.named_scope('all reduce'):
+    out = jax.lax.psum(local_out, 'y')
+  return out
+
+def pperm_xla_matmul2(lhs: jax.Array, rhs: jax.Array) -> jax.Array:
+  """
+  In this scenario, we realize that we actaully don't need to send the full
+  data both ways. That is, in our simple square, we recognize that only the
+  off-diagonal devices _need_ the data
+  """
+  this_x = jax.lax.axis_index('x')
+  this_y  = jax.lax.axis_index('y')
+  # Swap the shards along the x mesh axis
+  with jax.named_scope('ppermute'):
+    rhs_remote = jax.lax.ppermute(rhs, 'x', perm=[(0, 1), (1, 0)])
+
+  with jax.named_scope('build rhs'):
+    w_slice = jax.lax.select(this_x == this_y, rhs, rhs_remote)
+  
+  with jax.named_scope('local gemm'):
+    out = jax.lax.psum(lhs @ w_slice, 'y')
+
   return out
